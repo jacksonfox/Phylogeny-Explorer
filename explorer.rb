fdf04a69bcc7205dac3636a875212b37397e8310
Jackson Fox <jacksonfox@gmail.com>
2011-10-13 16:05:34 -0500

    Ripped out distances code :(, added /generate end point for tree creation, now passing list of excluded species along with selected species

--- explorer.rb
+++ explorer.rb
@@ -10,7 +10,7 @@ DATA_DIR = '/public/data/'
 DATA_URL = '/data/'
 
 class Collection
-  attr_accessor :species, :distances, :name, :url
+  attr_accessor :species, :name, :url, :distances_file
   
   def initialize(collection_name)
     config_file = File.join(Dir.pwd, DATA_DIR, collection_name, 'config.yml')
@@ -22,7 +22,7 @@ class Collection
     base_path = File.join(Dir.pwd, DATA_DIR, collection_name)
 
     species_file = File.join(base_path, config['species_file'])
-    distances_file = File.join(base_path, config['distances_file'])
+    @distances_file = File.join(base_path, config['distances_file'])
         
     @species = []
     @distances = []
@@ -30,38 +30,7 @@ class Collection
     CSV.foreach(species_file) do |s|
       @species << CollectionSpecies.new(s, self)
     end    
-    
-    CSV.foreach(distances_file) do |d|
-      @distances << {
-        :s1 => d[0],
-        :s2 => d[1],
-        :distance => d[2]
-      }
-    end    
   end
-  
-  def distance_between(s1, s2)
-    distance_pair = @distances.select { |d| (d[:s1] == s1 && d[:s2] == s2) || d[:s2] == s1 && d[:s1] == s2 }
-    distance_pair.first[:distance]
-  end
-  
-  def distances_from(s1, *other_species)
-    distances_from = []
-    other_species.each do |s2|
-      distances_from << [s2.name, distance_between(s1.name, s2.name)]    
-    end
-    distances_from
-  end
-  
-  # todo: screws up the previous method for some reason
-  # def distances_from(s1)
-  #   distances_from = []
-  #   other_species = @species.select { |s2| s2 != s1 }
-  #   other_species.each do |s2|
-  #     distances_from << [s2.name, distance_between(s1.name, s2.name)]
-  #   end
-  #   distances_from
-  # end
 end
 
 class CollectionSpecies
@@ -95,11 +64,20 @@ get '/explore' do
   erb :explore
 end
 
+post '/generate' do
+  distances_file = params[:file]
+  exclude_species = params[:exclude]
+  status 200
+  output = `bin/make_tree.pl --file #{distances_file} --exclude #{exclude_species}`
+  body(output)
+end
+
 post '/view' do
   @collection = collection
   unless params[:species].nil?
     species_ids = params[:species].collect {|s| s[0].to_i}
     @species = collection.species.select {|s| species_ids.include?(s.id)}
+    @exclude = collection.species.select {|s| !species_ids.include?(s.id)}
   end
   erb :view
 end
