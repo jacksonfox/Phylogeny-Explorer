c81ba302c6792e878de9635dcd3dc402c81178d7
Jackson Fox <jacksonfox@gmail.com>
2011-10-13 21:17:01 -0500

    Switched to using -keep_id instead of -remove_id in call to splice(), but had to add reroot() call to prevent a nasty bug

--- bin/make_tree.pl
+++ bin/make_tree.pl
@@ -4,19 +4,22 @@ use Getopt::Long;
 use Bio::Tree::DistanceFactory;
 use Bio::Matrix::IO;
 use Bio::TreeIO;
+use Data::Dumper;
 
-my $distances_file = '';
-my @exclude_species = ();
+my $distances_file;
+my @species;
 
-GetOptions('file=s' => \$distances_file, 'exclude=s' => \@exclude_species);
-@exclude_species = split(/,/,join(',',@exclude_species));
+GetOptions('file=s' => \$distances_file, 'species=s' => \@species);
+@species = split(/,/,join(',',@species));
 
-my $dfactory = Bio::Tree::DistanceFactory->new(-method => 'NJ');
+my $dfactory = Bio::Tree::DistanceFactory->new(-method => 'UPGMA');
 my $treeout = Bio::TreeIO->new(-format => 'newick');
 my $parser = Bio::Matrix::IO->new(-format => 'phylip', -file => $distances_file);
                                   
 my $matrix  = $parser->next_matrix;
 my $tree = $dfactory->make_tree($matrix);
 
-$tree->splice(-remove_id => \@exclude_species);
+$tree->reroot($tree->find_node(@species[0]));
+$tree->splice(-keep_id => \@species);
+
 $treeout->write_tree($tree); print "\n";
