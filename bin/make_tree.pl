103e9490e504c6087d556a90815061ab26938fa0
Jackson Fox <jacksonfox@gmail.com>
2011-10-23 22:53:54 -0500

    Now generating .dist file based on selected species and passing it to make_tree.pl, pruning the tree wasn't working out very well"

--- bin/make_tree.pl
+++ bin/make_tree.pl
@@ -4,13 +4,10 @@ use Getopt::Long;
 use Bio::Tree::DistanceFactory;
 use Bio::Matrix::IO;
 use Bio::TreeIO;
-use Data::Dumper;
 
 my $distances_file;
-my @species;
 
-GetOptions('file=s' => \$distances_file, 'species=s' => \@species);
-@species = split(/,/,join(',',@species));
+GetOptions('file=s' => \$distances_file);
 
 my $dfactory = Bio::Tree::DistanceFactory->new(-method => 'UPGMA');
 my $treeout = Bio::TreeIO->new(-format => 'newick');
@@ -18,8 +15,5 @@ my $parser = Bio::Matrix::IO->new(-format => 'phylip', -file => $distances_file)
                                   
 my $matrix  = $parser->next_matrix;
 my $tree = $dfactory->make_tree($matrix);
-
-$tree->reroot($tree->find_node(@species[0]));
-$tree->splice(-keep_id => \@species);
 
 $treeout->write_tree($tree); print "\n";
--- explorer.rb
+++ explorer.rb
@@ -6,11 +6,12 @@ require_relative 'partials'
 
 helpers Sinatra::Partials
 
+TEMP_DIR = '/public/tmp/'
 DATA_DIR = '/public/data/'
 DATA_URL = '/data/'
 
 class Collection
-  attr_accessor :species, :name, :url, :distances_file
+  attr_accessor :species, :name, :url, :distances
   
   def initialize(collection_name)
     config_file = File.join(Dir.pwd, DATA_DIR, collection_name, 'config.yml')
@@ -22,7 +23,7 @@ class Collection
     base_path = File.join(Dir.pwd, DATA_DIR, collection_name)
 
     species_file = File.join(base_path, config['species_file'])
-    @distances_file = File.join(base_path, config['distances_file'])
+    distances_file = File.join(base_path, config['distances_file'])
         
     @species = []
     @distances = []
@@ -30,6 +31,36 @@ class Collection
     CSV.foreach(species_file) do |s|
       @species << CollectionSpecies.new(s, self)
     end    
+    
+    CSV.foreach(distances_file) do |d|
+      @distances << {
+        :s1 => d[0],
+        :s2 => d[1],
+        :distance => d[2]
+      }
+    end
+  end
+  
+  def distance_between(s1, s2)
+    distance = 0.0
+    if (s1 != s2)
+      distance_pair = @distances.select { |d| (d[:s1] == s1 && d[:s2] == s2) || d[:s2] == s1 && d[:s1] == s2 }
+      distance = distance_pair.first[:distance]
+    end
+    return distance
+  end
+  
+  def distances_from(s1, *other_species)
+    distances_from = []
+    other_species.each do |s2|
+      distances_from << [s2.name, distance_between(s1.name, s2.name)]
+    end
+    return distances_from
+  end
+  
+  def find_by_id(id)
+    species = @species.select{ |s| s.id == id }
+    return species.first
   end
 end
 
@@ -51,6 +82,26 @@ class CollectionSpecies
 end
 
 helpers do
+  def generate_tempfile_name(prefix=nil, suffix=nil)
+    t = Time.now.strftime("%Y%m%d")
+    filename = "#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}#{suffix}"
+  end
+  def write_distances_file(collection, *species_ids)    
+    output_file = File.join(Dir.pwd, TEMP_DIR, generate_tempfile_name(nil,".dist"))
+    output = File.open(output_file, 'w')
+    species = species_ids.map{ |s| collection.find_by_id(s) }
+
+    # Output distance matrix in phylip format
+    output.puts " #{species.count}"
+    species.each do |s|
+      from_each = collection.distances_from(s, *species).map{|s2| s2[1].to_s}
+      output.puts "#{s.name} #{from_each.join(' ').strip}"
+    end
+    
+    # Close the IO to make sure buffer is flushed, then return path to file
+    output.close    
+    return output_file
+  end
 end
 
 collection = Collection.new('primates')
@@ -65,10 +116,10 @@ get '/explore' do
 end
 
 post '/generate' do
-  distances_file = params[:file]
-  species = params[:species]
+  species_ids = params[:species].split(',').map{ |id| id.to_i }
+  distances_file = write_distances_file(collection, *species_ids)
   status 200
-  output = `bin/make_tree.pl --file #{distances_file} --species #{species}`
+  output = `bin/make_tree.pl --file #{distances_file}`
   body(output)
 end
 
